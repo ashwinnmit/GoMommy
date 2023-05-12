dc676fd90c6b09792b7c842153ab1db2587a0888
Ashwin <ashwinrb74@gmail.com>
2023-05-12 19:34:21 +0530

    Smart Contract created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import DisplayDomains from './components/DisplayDomains';
+import GoMommy from "./abis/GoMommy.json";
+import { useEffect, useState } from 'react';
+// import DisplayDomains from './components/DisplayDomains';
 
 function App() {
+  var [account, changeAccount] = useState(null);
+  const loadBlockchainData = async()=>{
+     const accounts = await window.ethereum.request({'method':'eth_requestAccounts'})
+     var acc = accounts[0];
+     changeAccount(acc);
+    //  console.log(account);
+  }
+  useEffect(()=>{
+    loadBlockchainData();
+  }, []);
   return (
     <>
-      <DisplayDomains />
+      <h1>Hello World</h1>
+      <p>This is {account}</p>
     </>
   );
 }
